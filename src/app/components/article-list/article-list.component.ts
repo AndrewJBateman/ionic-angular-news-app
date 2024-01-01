ad72ddf675bbd5ef1af03663c92acf8b3bde93f4
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-01 21:21:32 +0100

    refactor: use latest control flow, move styles from template

--- src/app/components/article-list/article-list.component.ts
+++ src/app/components/article-list/article-list.component.ts
@@ -1,13 +1,21 @@
+/**
+ * ArticleListComponent displays a list of Article data.
+ *
+ * @Component decorator defines this as an Angular component.
+ * - selector: Custom HTML element to use this component.
+ * - templateUrl: Points to the component's HTML template.
+ * - styleUrls: Points to the component's CSS styles.
+ *
+ * @Input() article: Binds an Article object to display in the template.
+ */
 import { Component, Input } from "@angular/core";
 import { Article } from "../../interfaces/interfaces";
 
 @Component({
-	selector: "app-article-list",
-	templateUrl: "./article-list.component.html",
-	styleUrls: ["./article-list.component.scss"],
+  selector: "app-article-list",
+  templateUrl: "./article-list.component.html",
+  styleUrls: ["./article-list.component.scss"],
 })
 export class ArticleListComponent {
-	@Input() article: Article;
-
-	constructor() {}
+  @Input() article: Article | null;
 }
